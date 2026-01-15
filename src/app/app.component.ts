537f4fa77b272ba6719aa712edd40c7f9511f56b
Amit68568 <amit68568@gmail.com>
2026-01-15 15:09:37 +0530

    Upgrade angular version 13 to 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, MainComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'suraj-investment';
